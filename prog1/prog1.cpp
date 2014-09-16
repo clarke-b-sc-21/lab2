59f2a4f8a9575b5d8ab038cc509718e475b87d84
Logan Clarke <clarke.b.sc@live.ca>
2014-09-16 12:35:44 -0600

    changed prog1.cpp &makefile

--- prog1/prog1.cpp
+++ prog1/prog1.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
-
 using namespace std;
 
+/*
+
+Author: Logan Clarke
+Lab Assignment #2
+Purpose: This program calculates the roots of a quadratic equation using 
+the infamous quadratic formula. It asks the user for three coefficients (a,b, & c);
+and displays the two roots. 
+*/
+
+
+
 int main() {
+
+
+
+double a,b,c,r1,r2;
+cout<< "Please enter the first coefficient: " << endl;
+cin>> a >> endl;
+cout<< "Please enter the second coefficient: " << endl;
+cin>> b>> endl;
+cout<< "Please enter the third coefficient: " << endl;
+cin>> c >> endl; 
+
+  r1= ( (-b) + (sqrt( (b*b) - (4*a*c) ))  ) / (2*a)  
+  r2= ( (-b) - (sqrt( (b*b) - (4*a*c) ))  ) / (2*a)
+
+cout<< " The first root is: "  << r1 << endl;
+cout<< " The second root is: "  << r2 << endl;
+
 	return 0;
+
+
 }
